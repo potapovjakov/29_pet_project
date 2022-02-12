565f507ed255027e813a36288eceb0270897ce76
Alexander Khmelevsky <a.khmelevsky.255@gmail.com>
2022-02-12 22:53:50 +0300

    Fix pre-push commit

--- yatinder/yatinder/settings/base.py
+++ yatinder/yatinder/settings/base.py
@@ -129,4 +129,4 @@ class Base(Configuration):
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
-    CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+    # CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
